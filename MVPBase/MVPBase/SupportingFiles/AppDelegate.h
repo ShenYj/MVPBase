15bd8e2d51cf0010c044c3f05b03416759287859
ShenYj <shenyj4@51nb.com>
2018-12-15 12:15:05 +0800

    重构目录结构

---
+++ MVPBase/MVPBase/SupportingFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVPBase
+//
+//  Created by ShenYj on 2018/12/15.
+//  Copyright © 2018 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
